2818c82566a6a70d70a26dbf346a2b89991fd574
Hammed Akande <48578759+drhammed@users.noreply.github.com>
2024-07-15 20:17:41 -0400

    0.1.2

--- gdUpload/gdhandler.py
+++ gdUpload/gdhandler.py
@@ -40,7 +40,8 @@ class GoogleDriveHandler:
                 try:
                     flow = InstalledAppFlow.from_client_secrets_file(self.credentials_path, self.SCOPES)
                     # Check if running in Google Colab
-                    if 'COLAB_GPU' in os.environ or 'JPY_PARENT_PID' in os.environ:  
+                    #if 'COLAB_GPU' in os.environ or 'JPY_PARENT_PID' in os.environ:
+                    if 'COLAB_GPU' in os.environ in os.environ:   
                         creds = flow.run_console()
                     else:
                         creds = flow.run_local_server(port=0)
